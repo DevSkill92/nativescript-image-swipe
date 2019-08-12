4a96d37c4cdcd14f76a927a01a87b1d3ebb6464c
DieSoftwareentwickler <info@diesoftwareentwickler.de>
2019-08-12 08:51:11 +0200

    removed demo app

--- demo-ng/app/main.ts
+++
@@ -1,9 +0,0 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-import { registerElement } from "nativescript-angular/element-registry";
-
-import { AppModule } from "./app.module";
-
-registerElement("ImageSwipe", () => require("nativescript-image-swipe/image-swipe").ImageSwipe);
-
-platformNativeScriptDynamic().bootstrapModule(AppModule);
